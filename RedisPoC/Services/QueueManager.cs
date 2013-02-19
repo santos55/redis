af4bbab67f5d32b23fa241d21e8a4181c22583f9
santos55 <santiago.ortino@gmail.com>
2013-02-19 11:36:04 -0300

    Commit

---
+++ RedisPoC/Services/QueueManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks; 
+using BookSleeve;
+
+namespace Services
+{
+    public class QueueManager
+    {
+        private const int db = 12;
+
+        public void Set(string key, string value)
+        {
+            using (var conn = new RedisConnection("localhost"))
+            {
+                conn.Open();
+                conn.Set(db, key, value);
+            }
+        }
+
+        public async Task<string> Get(string key)
+        {
+            using (var conn = new RedisConnection("localhost"))
+            {
+                await conn.Open();
+                var value = await conn.GetString(db, key);
+                return value;
+            }
+
+
+            
+        }
+
+
+
+    }
+}
---
+++ RedisPoC/TestConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Services;
+
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var queue = new QueueManager();
+
+            queue.Set("key1", "value1");
+            Console.WriteLine(queue.Get("key1").Result);
+
+            Console.ReadKey();
+        }
+    }
+}
