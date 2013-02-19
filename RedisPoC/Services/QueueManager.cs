242d8fb276aaea51a444364f917613ba3fb75f23
santos55 <santiago.ortino@gmail.com>
2013-02-19 18:34:40 -0300

    Added list management (WIP)

--- RedisPoC/Services/QueueManager.cs
+++ RedisPoC/Services/QueueManager.cs
@@ -9,29 +9,94 @@ namespace Services
     {
         private const int db = 12;
 
+
+        public void Flush()
+        {
+            using (var conn = new RedisConnection("localhost"))
+            {
+                throw new NotImplementedException();
+            }
+
+        }
+        /// <summary>
+        /// Sets a single value for a key.
+        /// </summary>
         public void Set(string key, string value)
         {
             using (var conn = new RedisConnection("localhost"))
             {
                 conn.Open();
-                conn.Set(db, key, value);
+                conn.Strings.Set(db, key, value);
             }
+            
         }
 
-        public async Task<string> Get(string key)
+        /// <summary>
+        /// Checks if key exists.
+        /// </summary>
+        public async Task<bool> Exists(string key)
         {
             using (var conn = new RedisConnection("localhost"))
             {
                 await conn.Open();
-                var value = await conn.GetString(db, key);
+                return await conn.Keys.Exists(db, key);
+            }
+        }
+        
+        /// <summary>
+        /// Gets a single value for the specified key.
+        /// </summary>
+        public async Task<string> GetAsync(string key)
+        {
+            using (var conn = new RedisConnection("localhost"))
+            {
+                await conn.Open();
+                var value = await conn.Strings.GetString(db, key);
                 return value;
             }
-
-
-            
         }
 
+        public void AddListHead(string key, string value)
+        {
+            using (var conn = new RedisConnection("localhost"))
+            {
+                conn.Open();
+                conn.Lists.AddFirst(db, key, value);
+            }
+        }
 
+        public void AddListTail(string key, string value)
+        {
+            using (var conn = new RedisConnection("localhost"))
+            {
+                conn.Open();
+                conn.Lists.AddLast(db, key, value);
+            }
+        }
 
+        /// <summary>
+        /// Gets a single value for the specified key.
+        /// </summary>
+        public async Task<string> GetListHeadAsync(string key)
+        {
+            using (var conn = new RedisConnection("localhost"))
+            {
+                await conn.Open();
+                var value = await conn.Lists.GetString(db, key, 0);
+                return value;
+            }
+        }
+        /// <summary>
+        /// Gets a single value for the specified key.
+        /// </summary>
+        public async Task<string> GetListTailAsync(string key)
+        {
+            using (var conn = new RedisConnection("localhost"))
+            {
+                await conn.Open();
+                var value = await conn.Lists.GetString(db, key, -1);
+                return value;
+            }
+        }
     }
 }
--- RedisPoC/TestConsole/Program.cs
+++ RedisPoC/TestConsole/Program.cs
@@ -15,8 +15,33 @@ namespace TestConsole
 
             var queue = new QueueManager();
 
-            queue.Set("key1", "value1");
-            Console.WriteLine(queue.Get("key1").Result);
+            const string key1 = "key1";
+
+            Console.WriteLine("{0} exists: {1}", key1, queue.Exists(key1).Result);
+            queue.Set(key1, "value1");
+            Console.WriteLine("{0} set", key1);
+            Console.WriteLine("{0} exists: {1}", key1, queue.Exists(key1).Result);
+            Console.WriteLine(queue.GetAsync(key1).Result);
+
+
+            const string listKey1 = "listKey1";
+            Console.WriteLine("{0} exists: {1}", listKey1, queue.Exists(listKey1).Result);
+
+            queue.AddListHead(listKey1, "value1");
+            Console.WriteLine("{0} head is: {1}", listKey1, queue.GetListHeadAsync(listKey1).Result);
+
+            queue.AddListHead(listKey1, "value2");
+            Console.WriteLine("{0} head is: {1}", listKey1, queue.GetListHeadAsync(listKey1).Result);
+
+            queue.AddListHead(listKey1, "value3");
+            Console.WriteLine("{0} head is: {1}", listKey1, queue.GetListHeadAsync(listKey1).Result);
+
+            queue.AddListHead(listKey1, "value4");
+            Console.WriteLine("{0} head is: {1}", listKey1, queue.GetListHeadAsync(listKey1).Result);
+            
+            queue.AddListTail(listKey1, "value0");
+            Console.WriteLine("{0} head is: {1}", listKey1, queue.GetListHeadAsync(listKey1).Result);
+            Console.WriteLine("{0} tails is: {1}", listKey1, queue.GetListTailAsync(listKey1).Result);
 
             Console.ReadKey();
         }
